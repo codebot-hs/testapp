4213646d048bac6be9504b178030d50ce34fffb1
Cody Husek <chusek@jahnelgroup.com>
2020-01-27 14:51:01 -0500

    Furthered tutorial

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+allowNewServer = false;
+serverCreationStatus = 'No server was created';
+serverName = 'Test Server';
+serverCreated = false;
+servers = ['Testserver', 'Testserver 2']
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true
+    },2000)
+   }
 
   ngOnInit() {
   }
 
+  onCreateServer() {
+    this.serverCreated = true;
+    this.serverCreationStatus = 'Server was created: ' + this.serverName;
+    this.servers.push(this.serverName)
+  }
 }
