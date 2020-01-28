9a083c2ea94480dfc64a9e272ec43f705d085c4a
Cody Husek <chusek@jahnelgroup.com>
2020-01-28 16:52:41 -0500

    Added new course components

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,6 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
+  recipes: [Recipe] = [
+    new Recipe('Test Recipe',
+              'This is simply a test',
+              'https://live.staticflickr.com/65535/48436448591_9dc8a78ee2_b.jpg'),
+              new Recipe('Test Recipe',
+              'This is simply a test',
+              'https://live.staticflickr.com/65535/48436448591_9dc8a78ee2_b.jpg'),
+  ];
+   
   constructor() { }
 
   ngOnInit() {
